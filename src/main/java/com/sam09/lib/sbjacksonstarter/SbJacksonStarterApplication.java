891d434c986a79c3ccd95400cd5317ddb2130887
Soumyabrata Sen <soumyabratasen@hotmail.com>
2021-08-01 15:28:42 +0530

    Jackson dependency added to pom.xml

---
+++ src/main/java/com/sam09/lib/sbjacksonstarter/SbJacksonStarterApplication.java
@@ -0,0 +1,13 @@
+package com.sam09.lib.sbjacksonstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJacksonStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJacksonStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sam09/lib/sbjacksonstarter/SbJacksonStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam09.lib.sbjacksonstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJacksonStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
